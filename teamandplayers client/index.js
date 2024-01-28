4ae92fba8b88366824e6412e8ff26aff865ca5a9
medrare.oumaima <oumaima.medrare@gmail.com>
2024-01-28 19:36:54 +0100

    sloving minor error

--- teamandplayers client/index.js
+++ teamandplayers client/index.js
@@ -16,8 +16,7 @@ async function createTeam(teamData) {
     console.log("Team created:", response.data);
     return response.data;
   } catch (error) {
-    console.error("Error creating team:", error.response.data);
-    throw error;
+    handleError(error);
   }
 }
 
@@ -28,8 +27,7 @@ async function getAllTeams() {
     console.log("All teams:", response.data);
     return response.data;
   } catch (error) {
-    console.error("Error getting teams:", error.response.data);
-    throw error;
+    handleError(error);
   }
 }
 
@@ -43,8 +41,7 @@ async function updateTeam(teamId, updatedTeamData) {
     console.log("Team updated:", response.data);
     return response.data;
   } catch (error) {
-    console.error("Error updating team:", error.response.data);
-    throw error;
+    handleError(error);
   }
 }
 
@@ -55,8 +52,7 @@ async function deleteTeam(teamId) {
     console.log("Team deleted:", response.data);
     return response.data;
   } catch (error) {
-    console.error("Error deleting team:", error.response.data);
-    throw error;
+    handleError(error);
   }
 }
 
@@ -67,6 +63,7 @@ const createPlayer = async (player) => {
   try {
     const response = await axios.post(`${baseUrl}/players`, player);
     console.log("Player created:", response.data);
+    return response.data;
   } catch (error) {
     handleError(error);
   }
@@ -77,6 +74,7 @@ const getAllPlayers = async () => {
   try {
     const response = await axios.get(`${baseUrl}/players`);
     console.log("All Players:", response.data);
+    return response.data;
   } catch (error) {
     handleError(error);
   }
@@ -87,6 +85,7 @@ const getPlayerById = async (playerId) => {
   try {
     const response = await axios.get(`${baseUrl}/players/${playerId}`);
     console.log("Player:", response.data);
+    return response.data;
   } catch (error) {
     handleError(error);
   }
@@ -100,6 +99,7 @@ const updatePlayer = async (playerId, updatedPlayer) => {
       updatedPlayer
     );
     console.log("Player updated:", response.data);
+    return response.data;
   } catch (error) {
     handleError(error);
   }
@@ -110,6 +110,7 @@ const deletePlayer = async (playerId) => {
   try {
     const response = await axios.delete(`${baseUrl}/players/${playerId}`);
     console.log("Player deleted:", response.data);
+    return response.data;
   } catch (error) {
     handleError(error);
   }
@@ -124,24 +125,25 @@ async function main() {
   };
 
   const createdTeam = await createTeam(newTeam);
-  getAllTeams();
+  await getAllTeams();
   const updatedTeam = await updateTeam(createdTeam.t_id, {
     t_name: "Updated Team Name",
   });
   await deleteTeam(updatedTeam.t_id);
-  getAllTeams();
+  await getAllTeams();
 
   console.log("\n");
   console.log("CRUD players ");
+
   const newPlayer = { p_name: "New Player" };
-  createPlayer(newPlayer);
-  getAllPlayers();
-  const playerId = 1;
-  getPlayerById(playerId);
-  const updatedPlayer = { p_name: "Updated Player" };
-  updatePlayer(playerId, updatedPlayer);
-  deletePlayer(playerId);
-  getAllPlayers();
+
+  const createdPlayer = await createPlayer(newPlayer);
+  await getAllPlayers();
+  const updatedPlayer = await updatePlayer(createdPlayer.p_id, {
+    p_name: "Updated Player",
+  });
+  await deletePlayer(updatedPlayer);
+  await getAllPlayers();
 }
 
 main();
